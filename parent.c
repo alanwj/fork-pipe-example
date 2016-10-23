daaeb6ab033ec938c92c01304879697b201c80ca
Alan Johnson <alan.wayne.johnson@gmail.com>
2016-10-22 20:06:56 -0700

    Factor out fork/exec logic from the communication logic.

--- parent.c
+++ parent.c
@@ -7,92 +7,119 @@
 #include "io_helper.h"
 
 const char* messages[] = {
-  "This is a message!",
-  "This is another message.",
-  "This is a third message.",
+    "This is a message!",
+    "This is another message.",
+    "This is a third message.",
 };
 
-int main(void) {
-  fprintf(stderr, "parent: Starting\n");
-
-  // Create pipe to communicate with stdin.
+// Starts a child process.
+//
+// to_child_fd and from_child_fd will be set to file descriptors that may be
+// used to send data to or receive data from the child process.
+//
+// Returns the child pid on success, or -1 on failure.
+pid_t StartChild(const char* file, int* to_child_fd, int* from_child_fd) {
+  // Create pipe to connect to child's stdin.
   int in[2];
   if (pipe(in) == -1) {
     perror("parent: Failed to create stdin pipe");
-    return 1;
+    return -1;
   }
 
-  // Create pipe to communicate with stdout.
+  // Create pipe to connect to child's stdout.
   int out[2];
   if (pipe(out) == -1) {
     perror("parent: Failed to create stdout pipe");
-    return 1;
+    return -1;
   }
 
   pid_t pid = fork();
   if (pid == 0) {
     // We are in the child process.
 
-    // Child shouldn't write to stdin pipe.
+    // Child shouldn't write to its stdin.
     close(in[1]);
 
-    // Child shouldn't read from stdout pipe.
+    // Child shouldn't read from its stdout.
     close(out[0]);
 
     // Redirect stdin and stdout for the child process.
     if (dup2(in[0], fileno(stdin)) == -1) {
-      perror("parent: Failed to redirect stdin");
-      return 1;
+      perror("child[pre-exec]: Failed to redirect stdin for child");
+      return -1;
     }
     if (dup2(out[1], fileno(stdout)) == -1) {
-      perror("parent: Failed to redirect stdout");
-      return 1;
+      perror("child[pre-exec]: Failed to redirect stdout for child");
+      return -1;
     }
 
     // Start the child executable.
-    execlp("./child", "child", NULL);
+    execlp(file, file, NULL);
 
     // We should never get here.
-    perror("parent: Failed to start child");
-    return 1;
+    perror("child[pre-exec]: Failed to start child");
+    return -1;
   } else {
     // We are in the parent process.
     if (pid == -1) {
       perror("parent: Failed to create child");
+      return -1;
+    }
+
+    // Parent shouldn't read from child's stdin.
+    close(in[0]);
+
+    // Parent shouldn't write to child's stdout.
+    close(out[1]);
+
+    *to_child_fd = in[1];
+    *from_child_fd = out[0];
+    return pid;
+  }
+}
+
+int main(void) {
+  fprintf(stderr, "parent: Starting\n");
+
+  int to_child_fd;
+  int from_child_fd;
+  pid_t pid = StartChild("./child", &to_child_fd, &from_child_fd);
+  if (pid == -1) {
+    return 1;
+  }
+
+  size_t message_count = sizeof(messages) / sizeof(messages[0]);
+  char buf[1024];
+  for (size_t i = 0; i < message_count; ++i) {
+    size_t len = strlen(messages[i]);
+
+    // Write the length.
+    fprintf(stderr, "parent: Writing length   : %d\n", (int)len);
+    if (WriteFull(to_child_fd, &len, sizeof(len)) == -1) {
+      fprintf(stderr, "parent: Failed to write length.\n");
       return 1;
     }
 
-    // Parent shouldn't read from stdin pipe.
-    close(in[0]);
-
-    // Parent shouldn't write to stdout pipe.
-    close(out[1]);
-
-    size_t message_count = sizeof(messages) / sizeof(messages[0]);
-    char buf[1024];
-    for (size_t i = 0; i < message_count; ++i) {
-      size_t len = strlen(messages[i]);
-
-      // Write the length.
-      fprintf(stderr, "parent: Writing length   : %d\n", (int)len);
-      WriteFull(in[1], &len, sizeof(len));
-
-      // Write the data.
-      fprintf(stderr, "parent: Writing data     : %.*s\n", (int)len,
-              messages[i]);
-      WriteFull(in[1], messages[i], len);
-
-      // Read the response.
-      ReadFull(out[0], buf, len);
-      fprintf(stderr, "parent: Read response    : %.*s\n", (int)len, buf);
+    // Write the data.
+    fprintf(stderr, "parent: Writing message  : %.*s\n", (int)len, messages[i]);
+    if (WriteFull(to_child_fd, messages[i], len) == -1) {
+      fprintf(stderr, "parent: Failed to write message.\n");
+      return 1;
     }
 
-    // Tell child to quite.
-    size_t len = 0;
-    WriteFull(in[1], &len, sizeof(len));
-
-    fprintf(stderr, "parent: Waiting for child process to end.\n");
-    waitpid(pid, NULL, 0);
-    fprintf(stderr, "parent: Exiting\n");
+    // Read the response.
+    if (ReadFull(from_child_fd, buf, len) == -1) {
+      fprintf(stderr, "parent: Failed to read response.\n");
+      return 1;
+    }
+    fprintf(stderr, "parent: Read response    : %.*s\n", (int)len, buf);
   }
+
+  // Tell child to quite.
+  size_t len = 0;
+  WriteFull(to_child_fd, &len, sizeof(len));
+
+  fprintf(stderr, "parent: Waiting for child process to end.\n");
+  waitpid(pid, NULL, 0);
+  fprintf(stderr, "parent: Exiting\n");
 }
